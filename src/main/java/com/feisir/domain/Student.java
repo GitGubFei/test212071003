9ef1ae3156571e2871b60e7dc6a4b5918636a2d3
21207 <212074242@qq.com>
2021-10-03 11:02:45 +0800

    12

---
+++ src/main/java/com/feisir/domain/Student.java
@@ -0,0 +1,50 @@
+package com.feisir.domain;
+
+public class Student {
+
+    int stu_id;
+    String stu_name;
+    char stu_gender;
+    int stu_age;
+    String stu_phone;
+
+    public int getStu_id() {
+        return stu_id;
+    }
+
+    public void setStu_id(int stu_id) {
+        this.stu_id = stu_id;
+    }
+
+    public String getStu_name() {
+        return stu_name;
+    }
+
+    public void setStu_name(String stu_name) {
+        this.stu_name = stu_name;
+    }
+
+    public char getStu_gender() {
+        return stu_gender;
+    }
+
+    public void setStu_gender(char stu_gender) {
+        this.stu_gender = stu_gender;
+    }
+
+    public int getStu_age() {
+        return stu_age;
+    }
+
+    public void setStu_age(int stu_age) {
+        this.stu_age = stu_age;
+    }
+
+    public String getStu_phone() {
+        return stu_phone;
+    }
+
+    public void setStu_phone(String stu_phone) {
+        this.stu_phone = stu_phone;
+    }
+}
